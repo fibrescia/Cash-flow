50558414c94b6a526e60cbb62ca2b54d4b3077aa
fibrescia <fibrescia@gmail.com>
2023-01-13 20:52:08 -0300

    13-1-23 20:52

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const cargar_dinero = (event) => {
     let monto = Number(event.target.dinero__disponible.value)
     dinero_ingresado=monto
     /* let balance__izq=document.getElementById(`balance__izq`) */
-    balance__disponible.innerHTML=`<span class="verde">$ </span>${monto}`
+
+    balance__disponible.innerHTML=`<span class="verde">$ </span><span class="blanco">${monto}</span>`
+
     
 }
 const gastar_dinero = (event) => {
@@ -18,24 +20,25 @@ const gastar_dinero = (event) => {
     let monto = Number(event.target.monto__gasto.value)
     let nombre_gasto=event.target.nombre__gasto.value
     /* let gasto__total= (document.getElementById(`gasto__total`)) */
-    
+
     if(monto!=0){
     let lista_gastosHTML=document.getElementById(`lista_gastos`)
     let nuevo_gasto=document.createElement(`li`)
     nuevo_gasto.innerHTML= `<div class="contenedor_tacho_nombre">
     <i class="bi bi-trash"></i>
     <p class="lista__gasto_nombre" id="gasto_individual_nombre">${nombre_gasto}</p></div><p class="lista__gasto_monto">$ ${monto}</p>`
-    console.log(nuevo_gasto)
+    
 
     lista_gastosHTML.appendChild(nuevo_gasto)
     
     gasto_total+=monto
-    balance__gastos.innerHTML=`<span class="rojo">$ </span>${gasto_total}`
+    balance__gastos.innerHTML=`<span class="rojo">$ </span><span class="blanco">${monto}</span>`
     }
 }
 
 const calcular_balance = (event) => {
     event.preventDefault()
+
     let balance=dinero_ingresado-gasto_total
 
     balance>=0 ? balance__cuenta.innerHTML=`<span class="verde">$ ${balance}</span>` : balance__cuenta.innerHTML=`<span class="rojo">$ ${balance}</span>`
